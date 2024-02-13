ac6fb9f5585f70f4c379d4c97c9aad95204be489
@hexlet/cli <support@hexlet.io>
2024-02-13 09:05:30 +0300

    submit rails-basics-ru/models

--- rails-basics-ru/models/db/seeds.rb
+++ rails-basics-ru/models/db/seeds.rb
@@ -6,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+articles = []
+0.upto(10) { |i| articles << { title: "title_#{i}", body: "body_#{i}" } }
+Article.create(articles)
